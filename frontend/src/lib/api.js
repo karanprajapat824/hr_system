369b7479f991c687a94686a1ca9ac12cacfe91f1
karanprajapat824 <karanprajapat824@gmail.com>
2026-01-19 12:59:30 +0530

    Add backend url

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:4040/",
+    baseURL: "https://hr-system-3tks.onrender.com",
     withCredentials: true,
     headers: {
         "Content-Type": "application/json",
